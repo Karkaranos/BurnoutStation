18cccbc87c11f1603272cd3279f61e13d3d3987b
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-02-05 20:52:50 -0600

    Finalized PNG save and load system.
    Finalized the PNG saving system and implemented a way to sent images over the network, save them on client devices, and load those PNG files as sprites that can be displayed.

--- GraffitiGala/Assets/Scripts/ImageManagement/GraffitiPhotographer.cs
+++ GraffitiGala/Assets/Scripts/ImageManagement/GraffitiPhotographer.cs
@@ -1,39 +1,30 @@
 /*************************************************
 Brandon Koederitz
 1/31/2025
-2/2/2025
-Saves a list of meshes to a PNG file.
-NaughtyAttributes, IO
+2/5/2025
+Takes screenshots of the current camera view with transparency and 
+NaughtyAttributes, FishNet
 ***************************************************/
-
-using GraffitiGala.Drawing;
+using FishNet;
 using NaughtyAttributes;
-using System;
-using System.Collections.Generic;
-using System.IO;
+using UnityEditor;
 using UnityEngine;
 
 namespace GraffitiGala
 {
     [RequireComponent(typeof(Camera))]
-    public class GraffitiSaver : MonoBehaviour
+    public class GraffitiPhotographer : MonoBehaviour
     {
         #region vars
-        #region CONSTS
-        private const string FILE_FORMAT = ".png";
-        #endregion
-        [SerializeField, Tooltip("The name of the folder storing saved drawings.")]
-        private string _folderName = "Drawings";
-        [SerializeField, Tooltip("The base identifier name to give saved drawing files.")]
-        private string _defaultFileName = "GraffitiFile_";
         [SerializeField, Tooltip("Objects that should be excluded from saved images.")]
         private GameObject[] _hiddenObjects = new GameObject[0];
+        [SerializeField, Tooltip("Saves a copy of the saved image to this applications StreamingAssets folder.  " +
+            "Do not check if more than one instance of the application will be running.  For testing purposes only.")]
+        private bool _saveBackup;
 
         private static Camera captureCamera;
-        private static int captureNumber;
         private static bool isInitialized;
-        private static string folderName;
-        private static string defaultFileName;
+        private static bool saveBackup;
         private static GameObject[] hiddenObjects;
 
         #endregion
@@ -49,9 +40,8 @@ namespace GraffitiGala
                 {
                     captureCamera = foundCam;
                     isInitialized = true;
-                    folderName = _folderName;
-                    defaultFileName = _defaultFileName;
                     hiddenObjects = _hiddenObjects;
+                    saveBackup = _saveBackup;
                 }
             }
             else
@@ -60,30 +50,29 @@ namespace GraffitiGala
             }
         }
 
-        /// <summary>
-        /// Has the current GraffitiSaver take a screenshot of the current graffiti on the screen and save it to
-        /// StreamingAssets with an automatic file name.
-        /// </summary>
-        /// <returns>
-        /// The file path of the saved screenshot.
-        /// </returns>
-        [Button]
-        public static string ScreenshotDrawing()
-        {
-            // If no file name is given. the default file name is used with the date time appended.
-            string fileName = defaultFileName + DateTime.Now.Ticks;
-            return ScreenshotDrawing(fileName);
-        }
+        ///// <summary>
+        ///// Has the current GraffitiSaver take a screenshot of the current graffiti on the screen and save it to
+        ///// StreamingAssets with an automatic file name.
+        ///// </summary>
+        ///// <returns>
+        ///// The file path of the saved screenshot.
+        ///// </returns>
+        
+        //public static void ScreenshotDrawing()
+        //{
+        //    // If no file name is given. the default file name is used with the date time appended.
+        //    string fileName = defaultFileName + DateTime.Now.Ticks;
+        //    ScreenshotDrawing(fileName);
+        //}
 
         /// <summary>
         /// Has the current GraffitiSaver take a screenshot of the current graffiti on the screen and save it to
         /// StreamingAssets with a given file name.
         /// </summary>
-        /// <param name="fileName">The name of the screenshot file.</param>
-        /// <returns>The path of the saved screenshot.</returns>
-        public static string ScreenshotDrawing(string fileName)
+        [Button]
+        public static void ScreenshotDrawing()
         {
-            return ScreenshotDrawing(fileName, Screen.width, Screen.height);
+            ScreenshotDrawing(Screen.width, Screen.height);
         }
 
         /// <summary>
@@ -93,17 +82,20 @@ namespace GraffitiGala
         /// <remarks>
         /// Credit to Erik_Harg on the Unity Forums for this solution.
         /// </remarks>
-        /// <param name="fileName">The name of the screenshot file.</param>
         /// <param name="width"> The width in pixels of the screenshot to capture.</param>
         /// <param name="height">The height in pixels of the screenshot to capture.</param>
-        /// <returns>The path of the saved screenshot.</returns>
-        public static string ScreenshotDrawing(string fileName, int width, int height)
+        public static void ScreenshotDrawing(int width, int height)
         {
-            Debug.Log(width + " * " + height);
-
+            // Only allow the admin, which is always the server host, to save graffiti images.
+            if (!InstanceFinder.IsServerStarted)
+            {
+                Debug.LogError("Non-Server client attempted to save an image.  This is not allowed.");
+                return;
+            }
+            //Debug.Log(width + " * " + height);
             Camera cam = captureCamera != null ? captureCamera : Camera.main;
 
-            //Hides objects in the hiddenObjects array so they are not rendered in the screenshot.
+            // Hides objects in the hiddenObjects array so they are not rendered in the screenshot.
             foreach(GameObject go in hiddenObjects)
             {
                 if(go == null) { continue; }
@@ -129,8 +121,7 @@ namespace GraffitiGala
 
             // Save screenshot as .png
             byte[] fileData = screenshotTexture.EncodeToPNG();
-            string filePath = GetFilePath(fileName, folderName, true);
-            File.WriteAllBytes(filePath, fileData);
+            //File.WriteAllBytes(filePath, fileData);
 
             // Re-enables hidden objects.
             foreach(GameObject go in hiddenObjects)
@@ -144,31 +135,17 @@ namespace GraffitiGala
             Destroy(screenshotTexture);
             Destroy(screenshotRenderTexture);
 
-            Debug.Log("Saved a screenshot to " + filePath);
-
-            return filePath;    
-        }
-
-        /// <summary>
-        /// Returns the filepath for StreamingAssets given a specific file name.  Also creates relevant folders if 
-        /// they do not exist.
-        /// </summary>
-        /// <param name="fileName">The name of the file.</param>
-        /// <param name="fileDirectory">The folders within StreamingAssets to store this file at.</param>
-        /// <param name="createDirectory">
-        /// Whether to create new folders if they do not already exist.  If this option is set to false, you will get 
-        /// an error if you attempt to access a directory that does not exist.
-        /// </param>
-        /// <returns>The file path navigating to that file in StreamingAssets</returns>
-        private static string GetFilePath(string fileName, string fileDirectory, bool createDirectory)
-        {
-            string path = Path.Combine(Application.streamingAssetsPath, fileDirectory);
-            if(createDirectory)
+            // Broadcasts data containing the image file over the network so the application displaying the graffiti
+            // on buildings can save and use the given image.
+            ImageData imgData = new ImageData() {File = fileData};
+            InstanceFinder.ServerManager.Broadcast(imgData);
+            // Saves the recorded image to this device's streaming assets folder.
+            if(saveBackup)
             {
-                Directory.CreateDirectory(path);
+                ImageManagement.SaveImage(imgData, "GraffitiFile_");
             }
-            path = Path.Combine(path, fileName + FILE_FORMAT);
-            return path;
+
+            Debug.Log("Sent screenshot over the network.");
         }
     }
 
---
+++ GraffitiGala/Assets/Scripts/ImageManagement/ImageManagement.cs
@@ -0,0 +1,89 @@
+/*************************************************
+Brandon Koederitz
+2/5/2025
+2/5/2025
+Allows for the reading and writing of png image files via StreamingAssets
+IO
+***************************************************/
+
+using System.IO;
+using UnityEngine;
+
+namespace GraffitiGala
+{
+    public static class ImageManagement
+    {
+        #region vars
+        #region CONSTS
+        private const string FILE_FORMAT = ".png";
+        private const string FOLDER_NAME = "Drawings";
+        #endregion
+        private static string fileDirectory;
+        #endregion
+
+        #region Properties
+        public static string FileDirectory
+        {
+            get
+            {
+                if(fileDirectory == null)
+                {
+                    return Path.Combine(Application.streamingAssetsPath, FOLDER_NAME);
+                }
+                return fileDirectory;
+            }
+        }
+        #endregion
+        /// <summary>
+        /// Saves a given image to a folder in StreamingAssets specified by image data.
+        /// </summary>
+        /// <param name="imageData">The image of the graffiti that was taken by the server.</param>
+        /// <returns>The file path to the saved image.</returns>
+        public static string SaveImage(ImageData imageData, string fileName)
+        {
+            // Saves the passed in image data to a .png file at the drawing folder in StreamingAssets.
+            string filePath = GetFilePath(fileName, true);
+            File.WriteAllBytes(filePath, imageData.File);
+            return filePath;
+        }
+
+        /// <summary>
+        /// Returns the filepath for StreamingAssets given a specific file name.  Also creates relevant folders if 
+        /// they do not exist.
+        /// </summary>
+        /// <param name="fileName">The name of the file.</param>
+        /// <param name="createDirectory">
+        /// Whether to create new folders if they do not already exist.  If this option is set to false, you will get 
+        /// an error if you attempt to access a directory that does not exist.
+        /// </param>
+        /// <returns>The file path navigating to that file in StreamingAssets</returns>
+        private static string GetFilePath(string fileName, bool createDirectory = false)
+        {
+            if (createDirectory)
+            {
+                Directory.CreateDirectory(FileDirectory);
+            }
+            return Path.Combine(FileDirectory, fileName + FILE_FORMAT);
+        }
+
+        /// <summary>
+        /// Loads a sprite from the streamingAssets at a given file path.
+        /// </summary>
+        /// <param name="filePath">The file path to load the sprite from.</param>
+        /// <param name="pivotPoint">The normalized position of the sprite's pivot point.</param>
+        /// <param name="pixelsPerUnit">The pixel per unit to create the sprite with.</param>
+        /// <returns>The loaded image as a sprite object.</returns>
+        public static Sprite LoadSprite(string filePath, Vector2 pivotPoint, float pixelsPerUnit)
+        {
+            byte[] rawSpriteData = File.ReadAllBytes(filePath);
+            // Loads the raw image data to a Texture2D.
+            Texture2D spriteTexture = new Texture2D(0, 0);
+            spriteTexture.LoadImage(rawSpriteData);
+            // Creates a new sprite from the Texture2D.
+            Sprite sprite = Sprite.Create(spriteTexture, new Rect(0, 0, spriteTexture.width, spriteTexture.height),
+                pivotPoint, pixelsPerUnit);
+            return sprite;
+        }
+    }
+
+}
