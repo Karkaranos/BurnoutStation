e90ba04aa221c239e2551b14f4adf5d9ddbeb346
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-02-14 14:08:38 -0600

    Meeting test push 2

--- GraffitiGala/Assets/Scripts/ImageManagement/GraffitiReciever.cs
+++ GraffitiGala/Assets/Scripts/ImageManagement/GraffitiReciever.cs
@@ -47,6 +47,7 @@ namespace GraffitiGala
         {
             // Creates a new unique file name using the default file name and the current time.
             string fileName = defaultFileName + DateTime.Now.Ticks;
+            Debug.Log("Recieved image of name " + fileName);
             // Save the image to the folder in StreamingAssets.
             string filePath = ImageManagement.SaveImage(image, fileName);
             // Notify listeners that a new image has been saved.
