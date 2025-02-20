b7c5607a57b3619aa7261d2fcc1406707e0c39f8
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-02-20 11:29:36 -0600

    Button Drawing Bug
    Fixed a bug where drawings would still appear even if the player pressed a UI button.

--- GraffitiGala/Assets/Scripts/Drawing/Brushes/MeshNetBrush.cs
+++ GraffitiGala/Assets/Scripts/Drawing/Brushes/MeshNetBrush.cs
@@ -10,9 +10,9 @@ using FishNet.Connection;
 using FishNet.Object;
 using FishNet.Object.Synchronizing;
 using NaughtyAttributes;
-using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 namespace GraffitiGala.Drawing
@@ -163,17 +163,23 @@ namespace GraffitiGala.Drawing
         /// <param name="obj">Unused.</param>
         protected override void PressAction_Started(InputAction.CallbackContext obj)
         {
-            // Creates a new line and a new drawing state to link to that line.
-            DrawingState drawingState = new DrawingState(CreateNewLine(
-                brushTexturePrefab,
-                GetMousePosition(),
-                null,
-                CurrentColor
-                ), this);
-            // Sets the current state as the newly created drawing state.
-            state = drawingState;
-            // Adds the drawing state to the queue for initialization of it's line over the network.
-            drawingStateQueue.Add(drawingState);
+            //Debug.Log(EventSystem.current.IsPointerOverGameObject());
+            // Prevvents drawing if the pointer is over a UI element like a button.
+            // Will need to test if it works with drawing pens and touch input.
+            if (!EventSystem.current.IsPointerOverGameObject())
+            {
+                // Creates a new line and a new drawing state to link to that line.
+                DrawingState drawingState = new DrawingState(CreateNewLine(
+                    brushTexturePrefab,
+                    GetMousePosition(),
+                    null,
+                    CurrentColor
+                    ), this);
+                // Sets the current state as the newly created drawing state.
+                state = drawingState;
+                // Adds the drawing state to the queue for initialization of it's line over the network.
+                drawingStateQueue.Add(drawingState);
+            }
         }
 
         /// <summary>
