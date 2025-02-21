882e8e8db7394b32a672ae8c6137daf26bec36f1
Karkaranos <karkaranosyt@gmail.com>
2025-02-20 23:04:32 -0600

    basic sound implementation

---
+++ GraffitiGala/Assets/Scripts/Audio/FMODEventsManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using FMODUnity;
+
+public class FMODEventsManager : MonoBehaviour
+{
+
+    [field: Header("ServerMusic")]
+    [field: SerializeField] public EventReference BGM { get; private set; }
+
+    [field:Header("ServerSFX")]
+    [field: SerializeField] public EventReference Timer { get; private set; }
+    [field: SerializeField] public EventReference Ring { get; private set; }
+
+
+
+    [field: Header("ClientSFX")]
+    [field: SerializeField] public EventReference Spraypaint { get; private set; }
+
+    public static FMODEventsManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("There is more than one FMODEventsManager in the scene");
+            Destroy(instance);
+        }
+        instance = this;
+    }
+}
