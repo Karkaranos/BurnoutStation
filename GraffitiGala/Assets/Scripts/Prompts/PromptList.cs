d2f1f05d856076e12c08be76926bcfb0c843b1cd
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-02-23 16:01:35 -0600

    Basic Prompts
    Got a very basic system working to send prompts over the network.

---
+++ GraffitiGala/Assets/Scripts/Prompts/PromptList.cs
@@ -0,0 +1,26 @@
+/*************************************************
+Brandon Koederitz
+2/23/2025
+2/23/2025
+List of prompts that can be chosen to give to players.
+***************************************************/
+using UnityEngine;
+
+namespace GraffitiGala
+{
+    [CreateAssetMenu(fileName = "PromptList", menuName = "Scriptable Objects/Prompt List")]
+    public class PromptList : ScriptableObject
+    {
+        [SerializeField] private string[] prompts;
+
+        /// <summary>
+        /// Gets a random prompt from the prompts list.
+        /// </summary>
+        /// <returns>A random prompt.</returns>
+        public string GetRandomPrompt()
+        {
+            return prompts[Random.Range(0, prompts.Length)];
+        }
+    }
+
+}
