97939498c1cb85347e6c472258f33f57728f3447
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-04-16 17:16:49 -0500

    Graffiti Placement background 2nd iteration

--- GraffitiGala/Assets/Scripts/BuldingScripts/SpritePlaceVisualizer.cs
+++ GraffitiGala/Assets/Scripts/BuldingScripts/SpritePlaceVisualizer.cs
@@ -5,8 +5,7 @@ Brandon Koederitz
 Creates a visually large sprite on screen and scales it down to place it on it's renderer.  Purely a visual script.
 ***************************************************/
 using System.Collections;
-using System.Runtime.CompilerServices;
-using System.Threading;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 namespace GraffitiGala.City
@@ -30,10 +29,10 @@ namespace GraffitiGala.City
             }
 
             // Creates a visualizer with a given sprite and color.
-            void CreateVisualizer(Sprite visualizerSprite, Color color, int layerOffset, bool playSFX = false)
+            SpriteRenderer CreateVisualRenderer(Sprite visualizerSprite, Transform parent, Color color, int layerOffset = 0, float scaleModifier = 1f)
             {
                 GameObject go = new GameObject();
-                go.transform.SetParent(parentTransform, false);
+                go.transform.SetParent(parent, false);
                 // Set up the sprite renderer object.
                 SpriteRenderer sRend = go.AddComponent<SpriteRenderer>();
                 sRend.sprite = visualizerSprite;
@@ -43,17 +42,21 @@ namespace GraffitiGala.City
                 // The sprite will fade in as it appears on screen.
                 sRend.color = color;
                 sRend.color = sRend.color.SetAlpha(0);
-                SpritePlaceVisualizer visualizer = go.AddComponent<SpritePlaceVisualizer>();
-                go.transform.localScale = new Vector3(settings.SpriteMagnification, settings.SpriteMagnification, 1);
+
+                go.transform.localScale = new Vector3(settings.SpriteMagnification, settings.SpriteMagnification, 1)
+    * scaleModifier;
                 go.transform.localPosition = Vector3.zero;
-                // This makes the assumption that the camera we are using is the main camera.
-                visualizer.Tween(renderer, sRend, settings, Camera.main, playSFX);
+                return sRend;
             }
 
-            // Creates the visualizer for the graffiti itself.
-            CreateVisualizer(renderer.sprite, renderer.color, 0, true);
-            // Creates the visualizer for the background sprite.
-            CreateVisualizer(settings.BackgroundSprite, Color.white, -1, false);
+            // Creates the sprite renderer object for the graffiti itself.
+            SpriteRenderer graffitiRend = CreateVisualRenderer(renderer.sprite, parentTransform, renderer.color);
+            // Creates the sprite renderer object for the background sprite.
+            SpriteRenderer backgroundRend = CreateVisualRenderer(settings.BackgroundSprite, graffitiRend.transform, Color.white, -1, settings.BackgroundScaleMoifier);
+
+            SpritePlaceVisualizer visualizer = graffitiRend.gameObject.AddComponent<SpritePlaceVisualizer>();
+            // This makes the assumption that the camera we are using is the main camera.
+            visualizer.Tween(renderer, new SpriteRenderer[] { graffitiRend, backgroundRend }, settings, Camera.main);
         }
 
 
@@ -61,24 +64,24 @@ namespace GraffitiGala.City
         /// Tweens this object's transform to match the target transform.
         /// </summary>
         /// <param name="targetRenderer">The sprite renderer to copy.</param>
-        /// <param name="thisRenderer">The sprite renderer on this object.</param>
+        /// <param name="UpdateRenderers">The sprite renderer on this object.</param>
         /// <param name="settings">The settings to use for this sprite tween.</param>
         /// <param name="targetCam">The camera that this sprite will be displayed on.</param>
         /// <param name="playSFX">
         /// Whether this visualizer should play sound effects when it gets placed.  here to prevent double sounds from
         /// happening when a background is placed.
         /// </param>
-        private void Tween(SpriteRenderer targetRenderer, SpriteRenderer thisRenderer, 
-            SpriteVisualizerSettings settings, Camera targetCam, bool playSFX = false)
+        private void Tween(SpriteRenderer targetRenderer, SpriteRenderer[] UpdateRenderers, 
+            SpriteVisualizerSettings settings, Camera targetCam)
         {
-            StartCoroutine(TweenCoroutine(targetRenderer, thisRenderer, settings, targetCam, playSFX));
+            StartCoroutine(TweenCoroutine(targetRenderer, UpdateRenderers, settings, targetCam));
         }
 
         /// <summary>
         /// Tweens this object to match the target transform.
         /// </summary>
         /// <param name="targetRenderer">The sprite renderer to copy.</param>
-        /// <param name="thisRenderer">The sprite renderer on this object.</param>
+        /// <param name="updateRenderers">The sprite renderers on this object.</param>
         /// <param name="settings">The settings to use for this sprite tween.</param>
         /// <param name="targetCam">The camera that this sprite will be displayed on.</param>
         /// <param name="playSFX">
@@ -86,23 +89,32 @@ namespace GraffitiGala.City
         /// happening when a background is placed.
         /// </param>
         /// <returns>Coroutine.</returns>
-        private IEnumerator TweenCoroutine(SpriteRenderer targetRenderer, SpriteRenderer thisRenderer,
-            SpriteVisualizerSettings settings, Camera targetCam, bool playSFX = false)
+        private IEnumerator TweenCoroutine(SpriteRenderer targetRenderer, SpriteRenderer[] updateRenderers,
+            SpriteVisualizerSettings settings, Camera targetCam)
         {
             Transform targetTransform = targetRenderer.transform;
             // Hides the target sprite.
             targetRenderer.enabled = false;
+
+            void SetRendererColor(float value)
+            {
+                foreach(SpriteRenderer s in updateRenderers)
+                {
+                    s.color = s.color.SetAlpha(value);
+                }
+            }
 
             float fadeTimer = 0;
             // Continually fades in the sprite until it is fully visible.
             while (fadeTimer < settings.FadeInDuration)
             {
                 float normalizedTime = fadeTimer / settings.FadeInDuration;
-                thisRenderer.color = thisRenderer.color.SetAlpha(normalizedTime);
+
+                SetRendererColor(normalizedTime);
                 fadeTimer += Time.deltaTime;
                 yield return null;
             }
-            thisRenderer.color = thisRenderer.color.SetAlpha(1);
+            SetRendererColor(1);
 
             yield return new WaitForSeconds(settings.InitialDelay);
 
@@ -138,7 +150,7 @@ namespace GraffitiGala.City
             // Plays the graffiti highlight once it has been placed
             GraffitiHighlighter.SetHighlightedGraffiti(targetRenderer);
             // Play the end sound again.
-            if (BuildManager.BuildType == BuildType.CityDisplay && playSFX)
+            if (BuildManager.BuildType == BuildType.CityDisplay)
             {
                 AudioManager.instance.PlayOneShot(FMODEventsManager.instance.GraffitiDisplay, Vector3.zero);
             }
--- GraffitiGala/Assets/Scripts/BuldingScripts/SpriteVisualizerSettings.cs
+++ GraffitiGala/Assets/Scripts/BuldingScripts/SpriteVisualizerSettings.cs
@@ -5,6 +5,7 @@ Brandon Koederitz
 Set of information for visualizing a sprite tween.
 ***************************************************/
 
+using GameKit.Dependencies.Utilities;
 using UnityEngine;
 
 namespace GraffitiGala.City
@@ -19,8 +20,11 @@ namespace GraffitiGala.City
         public string SortingLayerName { get; private set; }
         [field: SerializeField]
         public int OrderInLayer { get; private set; }
+        [field: Header("Background Settings")]
         [field: SerializeField]
         public Sprite BackgroundSprite { get; private set; }
+        [field: SerializeField]
+        public float BackgroundScaleMoifier { get; private set; }
         [field: Header("Animation Settings")]
         [field: SerializeField, Tooltip("The amount of time the sprite tween should take.")]
         public float TweenTime { get; private set; }
